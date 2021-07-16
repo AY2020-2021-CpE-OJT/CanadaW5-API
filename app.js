09a91fe4159021503d053300b64b8d140a5145ba
DanielCanada <s1820987@usls.edu.ph>
2021-07-16 19:46:26 +0800

    note new additions

--- app.js
+++ app.js
@@ -20,20 +20,20 @@ app.get('/Week04', (req,res) => {
     res.send('This is to prompt that I have integrated my API to Heroku and that my application is able to execute the requirements! Thanks for viewing this page :)');
 })
 
-app.post('/api/login', (req, res) => {
-    //Guest User
-    const user = {
-        id: 1,
-        username = 'guest',
-        email: 'guest@gmail.com'
-    }
+//app.post('/api/login', (req, res) => {
+//    //Guest User
+//    const user = {
+//        id: 1,
+//        username = 'guest',
+//        email: 'guest@gmail.com'
+//    }
 
-    jwt.sign({user}, 'secretkeyguest', (err, token) =>{
-        res.json({
-            token
-        });
-    })
-});
+//   jwt.sign({user}, 'secretkeyguest', (err, token) =>{
+//        res.json({
+//            token
+//        });
+//    })
+//});
 
 //connect to mongoDB
 mongoose.connect(mongoConfig.MONGO_URI, {
